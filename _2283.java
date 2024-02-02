e7f21f410db8fbea9ec77808eb0e6d7105a97940
wonchul <shinwc1998@naver.com>
2024-02-02 18:00:28 +0900

    feat : _2283 solving

--- _2283.java
+++ _2283.java
@@ -1,4 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// TODO : 풀어야 해
 
 public class _2283 {
-
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N,K;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(br.readLine());
+		K = Integer.parseInt(br.readLine());
+	}
 }
