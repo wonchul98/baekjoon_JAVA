f2181b60c100bd493b50787ed7f9ff18f12e1944
sohyun <shinwc1998@naver.com>
2024-02-02 11:40:10 +0900

    feat : solve 14888, 1504

--- _14888.java
+++ _14888.java
@@ -1,17 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class _14888 {
 	public static int N;
 	public static int[] A = new int[11];
 	public static int[] op = new int[4]; // + - * /
-	public static void calc() {};
-	public static boolean nextPermutation(List<Character> L){
-		while
-
+	public static List<Integer> L = new ArrayList<>();
+	public static int calc() {
+		int idx = 0, num = A[0];
+		while(idx <= L.size()-1) {
+			switch(L.get(idx++)) {
+			case 0: {
+				num += A[idx];
+				break;
+			}
+			case 1 :{
+				num-=A[idx];
+				break;
+			}
+			case 2 : {
+				num*=A[idx];
+				break;
+			}
+			case 3 : {
+				if(num >= 0) {
+					num /= A[idx];
+					break;
+				} else {
+					num *= -1;
+					num /= A[idx];
+					num *= -1;
+					break;
+				}
+				
+			}
+			}
+		}
+		return num;
+	}
+	public static boolean nextPermutation(){
+		//System.out.println(L.toString());
+		int i = L.size() - 1;
+		while (i > 0 && L.get(i - 1) >= L.get(i))
+            --i;
+		if(i==0) return false;
+		int j = L.size()-1;
+		while(L.get(j) <= L.get(i-1)) 
+			--j;
+		Collections.swap(L, i-1, j);
+		int idx = L.size()-1;
+		while(i < idx) {
+			Collections.swap(L, i, idx);
+			i++;
+			idx--;
+		}
+		return true;
 	}
 	
 	public static void main(String[] args) throws IOException{
@@ -24,7 +72,20 @@ public class _14888 {
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0;i < 4;i++) {
 			op[i] = Integer.parseInt(st.nextToken());
+			
 		}
-		
+		for(int i = 0;i < 4; i++) {
+			for(int j = 0; j < op[i];j++) {
+				L.add(i);
+			}			
+		}
+		int maxAns = 0;
+		int minAns = Integer.MAX_VALUE;
+		do {
+			maxAns = Math.max(maxAns,  calc());
+			minAns = Math.min(minAns,  calc());
+		}while(nextPermutation());
+		System.out.println(maxAns);
+		System.out.println(minAns);
 	}
 }
--- _1504.java
+++ _1504.java
@@ -8,7 +8,7 @@ public class _1504 {
 	public static int N,E;
 	public static int[][] dist = new int[801][801];
 	public static boolean[][] visited = new boolean[801][801];
-	public static int magicNumber = Integer.MAX_VALUE / 3 - 1 ;
+	public static int magicNumber = Integer.MAX_VALUE;
 	public static void init() {
 		for(int i = 1; i <= N;i++) {
 			for(int j = 1;j <= N;j++) {
@@ -26,7 +26,7 @@ public class _1504 {
 		for(int i = 1;i <=N;i++) {
 			//System.out.printf("visited[%d][%d] : %b\n", a, i, visited[a][i]);
 			//System.out.printf("dist[%d][%d] : %d\n", a, i, dist[a][i]);
-			if(a != i && !visited[a][i] && dist[a][i] < min) {
+			if(a != i && !visited[a][i] && dist[a][i] < min) { //방문 안한 노드 중 연결이 되어있는 경우
 				index = i;
 				min = dist[a][i];
 			}
@@ -42,11 +42,14 @@ public class _1504 {
 		int cnt = N;
 		int nextNode = 0;
 		while(cnt-- > 0) {
-			nextNode = getSmallIndex(a);
+			nextNode = getSmallIndex(a); // 연결이 안된 인덱스는 안나옴
 			if(nextNode == N+1) continue;
 			//System.out.println("nextNode: " + nextNode);
 			for(int i = 1; i <= N;i++) {
-				dist[a][i] = Math.min(dist[a][nextNode] + dist[nextNode][i], dist[a][i]);
+				if(dist[nextNode][i] < magicNumber) {
+					dist[a][i] = Math.min(dist[a][nextNode] + dist[nextNode][i], dist[a][i]);
+					dist[i][a] = Math.min(dist[a][nextNode] + dist[nextNode][i], dist[a][i]);
+				}
 				//System.out.println(dist[a][i]);
 			}			
 		}
@@ -70,8 +73,15 @@ public class _1504 {
 		dijkstra(1);
 		dijkstra(A);
 		dijkstra(B);
-		int min = Math.min(dist[1][A] + dist[A][B] + dist[B][N], dist[1][B] + dist[B][A] + dist[A][N]);
-		if(min > magicNumber) System.out.println(-1);
-		else System.out.println(min);
+		int aCourse, bCourse;
+		if(dist[1][A] == magicNumber|| dist[A][B] == magicNumber || dist[B][N] == magicNumber) aCourse = magicNumber; 
+		else aCourse = dist[1][A] + dist[A][B] +  dist[B][N];
+		if(dist[1][B] == magicNumber|| dist[B][A] == magicNumber || dist[A][N] == magicNumber) bCourse = magicNumber; 
+		else bCourse  = dist[1][B]+dist[B][A]+dist[A][N];
+		if(aCourse == magicNumber && bCourse == magicNumber) System.out.println(-1);
+		else {
+			System.out.println(Math.min(aCourse, bCourse));
+		}
+		
 	}
 }
