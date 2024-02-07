f5ceca95484a6a3b16d958250bf2e8c092a02f48
wonchul98 <shinwc1998@naver.com>
2024-02-07 19:45:33 +0900

    s

--- _2457.java
+++ _2457.java
@@ -1,77 +1,79 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class _2457 {
-	public static int N;
-	public static INFO[] list;
-	public static int[] finalDay = {31,31,28,31,30,31,30,31,31,30,31,30,31};
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		list = new INFO[N];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int startMonth = Integer.parseInt(st.nextToken());
-			int startDay = Integer.parseInt(st.nextToken());
-			int endMonth = Integer.parseInt(st.nextToken());
-			int endDay = Integer.parseInt(st.nextToken());
-			list[i] = new INFO(startMonth, startDay, endMonth, endDay);
-		}
-		Arrays.sort(list);
-		int flag = list[0].endMonth;
-		List<INFO> newL = new ArrayList<>();
-		int idx = 1;
-		do {
-			if(list[idx].startMonth <= flag) {
-				newL.add(list[idx]);
-				idx++;
-				Collections.sort(newL, new Comparator<INFO>() {
-					@Override
-					public int compare(INFO o1, INFO o2) {
-							return o2.endMonth - o1.endMonth; // 내림차순
-					}
-				});
-			}
-		} while(!newL.isEmpty());
-		
-	}
-	public static class INFO implements Comparable<INFO>{
-		int startMonth;
-		int startDay;
-		int endMonth;
-		int endDay;
-		public INFO(int startMonth, int startDay, int endMonth, int endDay) {
-			if(endDay == 1) {
-				if(endMonth == 1) endMonth = 12;
-				else endMonth-=1;
-				endDay = finalDay[endMonth];
-			}
-			endDay -= 1;
-			this.startMonth = startMonth;
-			this.startDay = startDay;
-			this.endMonth = endMonth;
-			this.endDay = endDay;
-		}
-		@Override
-		public int compareTo(INFO o) {
-			if(this.startMonth == o.startMonth) {
-				if(this.endMonth == o.endMonth) return o.endDay - this.endDay;
-				return o.endMonth - this.endMonth;
-			}
-			return this.startMonth - o.startMonth;
-		}
-		@Override
-		public String toString() {
-			return "INFO [startMonth=" + startMonth + ", startDay=" + startDay + ", endMonth=" + endMonth + ", endDay="
-					+ endDay + "]";
-		}
-		
-	}
+    
+	public static int[] finalDay = {31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        INFO[] list = new INFO[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int startMonth = Integer.parseInt(st.nextToken());
+            int startDay = Integer.parseInt(st.nextToken());
+            int endMonth = Integer.parseInt(st.nextToken());
+            int endDay = Integer.parseInt(st.nextToken());
+            list[i] = new INFO(startMonth, startDay, endMonth, endDay);
+        }
+
+        Arrays.sort(list);
+
+        int covered = 300; //3월 1일
+        int maxCover = 0, count = 0, idx = 0;
+        boolean isPossible = false;
+
+        for (int i = 0; i < N ; i++) {
+        	if(covered >= 1101) break;
+            boolean updated = false;
+            while (idx < N && list[idx].start <= covered + 1) {
+                if (list[idx].end > maxCover) {
+                    maxCover = list[idx].end;
+                    updated = true;
+                    isPossible = true;
+                }
+                idx++;
+            }
+            if (updated) {
+                covered = maxCover;
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        if (covered < 1130) isPossible = false;
+
+        System.out.println(isPossible ? count : 0);
+    }
+    static class INFO implements Comparable<INFO> {
+        int start, end;
+        
+        public INFO(int startMonth, int startDay, int endMonth, int endDay) {
+            // 시작 날짜는 그대로 사용
+            this.start = startMonth * 100 + startDay;
+            // 종료 날짜를 하루 앞당겨 조정
+            if (endDay == 1) {
+                if (endMonth == 1) {
+                    this.end = 1231;
+                } else {
+                    endMonth--;
+                    this.end = endMonth * 100 + finalDay[endMonth];
+                }
+            } else {
+                this.end = endMonth * 100 + (endDay - 1);
+            }
+        }
+
+        @Override
+        public int compareTo(INFO o) {
+            if (this.start == o.start) {
+                return this.end - o.end;
+            }
+            return this.start - o.start;
+        }
+    }
 }
