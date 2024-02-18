6f9a2e9ca4533c81bf30b40398fe8cae23dc0430
wonchul98 <shinwc1998@naver.com>
2024-02-18 16:03:27 +0900

    feat : solved

--- _20166.java
+++ _20166.java
@@ -10,51 +10,7 @@ public class _20166 {
     static int [] dx = {-1,-1,0,1,1,1,0,-1}; // 상부터 시계방향
     static int [] dy = {0,1,1,1,0,-1,-1,-1};
     static int maxLen = Integer.MIN_VALUE;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        map = new char[N][M];
-        hm = new HashMap<>();
-
-        for(int i = 0; i < N; ++i)
-        {
-            String input = br.readLine();
-            for(int j = 0; j < M; ++j)
-            {
-                map[i][j] = input.charAt(j);
-            }
-        }
-
-        String [] keys = new String[K];
-        for(int i = 0; i < K; ++i)
-        {
-            // 좋아하는 문자열 해시맵에 0카운트로 put
-            String input = br.readLine();
-            //정답 출력을 위해 담아두기
-            keys[i] = input;
-            maxLen = Math.max(maxLen,input.length());
-            hm.put(input,0);
-        }
-
-        for(int i = 0; i < N; ++i)
-        {
-            for(int j = 0; j < M; ++j)
-            {
-                makeString(i,j);
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for(String s : keys)
-        {
-            sb.append(hm.get(s)).append("\n");
-        }
-        System.out.print(sb);
-    }
+    
     static void makeString(int x, int y)
     {
         Queue<Point> q = new LinkedList<>();
@@ -84,6 +40,52 @@ public class _20166 {
             }
         }
     }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        map = new char[N][M];
+        hm = new HashMap<>();
+
+        for(int i = 0; i < N; ++i)
+        {
+            String input = br.readLine();
+            for(int j = 0; j < M; ++j)
+            {
+                map[i][j] = input.charAt(j);
+            }
+        }
+
+        String [] keys = new String[K];
+        for(int i = 0; i < K; ++i)
+        {
+            String input = br.readLine();
+            keys[i] = input;
+            maxLen = Math.max(maxLen,input.length());
+            // 해시맵에 0카운트로 put
+            hm.put(input,0);
+        }
+
+        for(int i = 0; i < N; ++i)
+        {
+            for(int j = 0; j < M; ++j)
+            {
+                makeString(i,j);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String s : keys)
+        {
+            sb.append(hm.get(s)).append("\n");
+        }
+        System.out.print(sb);
+    }
+    
     public static class Point
     {
     	int x,y,len;
