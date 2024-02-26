5228ea7167fadd0fb104292481fc090519c4b2e2
wonchul98 <shinwc1998@naver.com>
2024-02-26 19:53:38 +0900

    feat : solve 3197

--- src/_3197.java
+++ src/_3197.java
@@ -1,7 +1,9 @@
+package src;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -9,56 +11,65 @@ public class _3197 {
 	public static int R, C, s_x, s_y, minCnt = Integer.MAX_VALUE, prt = 0;
 	public static char[][] map;
 	public static int[][] wMap;
+	public static int[][] dist;
 	public static boolean[][] visited;
 	public static int[] dx = {-1,0,1,0};
 	public static int[] dy = {0,1,0,-1};
 	
 	
-	public static int BFS() {
-		Queue<Pair> q = new LinkedList<>();
-		q.add(new Pair(s_x, s_y));
-		q.add(new Pair(-1,-1));
-		int todayMin = Integer.MAX_VALUE;
-		int rst = 0;
-		while(!q.isEmpty()) {
-			Pair cur = q.poll();
-			System.out.println("cur: " + cur);
-			if(cur.x == -1) {
-				if(q.isEmpty()) break;
-				rst = Math.max(rst, todayMin);
-				System.out.println("rst: " + rst);
-				todayMin = Integer.MAX_VALUE;
-				boolean update = false;
-				q.add(new Pair(-1, -1));
-			}
-			for(int i = 0; i < 4;i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				if(inRange(nx, ny) && !visited[nx][ny]) {
-					visited[nx][ny] = true;
-					q.add(new Pair(nx, ny));
-					if(wMap[nx][ny]!= 0)
-						todayMin = Math.min(todayMin, wMap[nx][ny]);
-				}
-			}
-		}
-		return rst;
-	}
+	public static int dijkstra() {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(s_x, s_y, 0));
+        dist[s_x][s_y] = 0;
+        int e_x = 0, e_y = 0;
+        while (!pq.isEmpty()) {
+            Node current = pq.poll();
+            int x = current.x;
+            int y = current.y;
+            int currentCost = current.cost;
+            if(map[x][y] == 'L') {
+            	e_x = x;
+            	e_y = y;
+            }
+
+            if (dist[x][y] < currentCost) {
+                continue;
+            }
+            
+            for (int i = 0;i < 4;i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
+                    int nextCost = Math.max(currentCost, wMap[nx][ny]);
+                    if (dist[nx][ny] > nextCost) {
+                        dist[nx][ny] = nextCost;
+                        pq.offer(new Node(nx, ny, nextCost));
+                    }
+                }
+            }
+            
+        }
+        return dist[e_x][e_y];
+    }
+	
 	public static boolean inRange(int x, int y) {
 		if(x < 0 || y < 0 || x >= R || y >= C) return false;
 		return true;
 	}
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		map = new char[R][C];
 		wMap = new int[R][C];
+		dist = new int[R][C];
 		visited = new boolean[R][C];
 		for(int i= 0;i < R;i++) {
 			String input = br.readLine();
 			for (int j = 0; j < C; j++) {
+				dist[i][j] = Integer.MAX_VALUE;
 				map[i][j] = input.charAt(j);
 				if(map[i][j] == 'L') {
 					s_x = i;
@@ -101,10 +112,10 @@ public class _3197 {
 				}
 			}
 		}
-		printMap();
+		//printMap();
 		visited = new boolean[R][C];
 		visited[s_x][s_y] = true;
-		System.out.println(BFS());
+		System.out.println(dijkstra());
 		
 	}
 	public static void printMap() {
@@ -127,6 +138,19 @@ public class _3197 {
 		public String toString() {
 			return "Pair [x=" + x + ", y=" + y + "]";
 		}
-		
 	}
+	static class Node implements Comparable<Node> {
+        int x, y, cost;
+
+        public Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.cost, other.cost);
+        }
+    }
 }
